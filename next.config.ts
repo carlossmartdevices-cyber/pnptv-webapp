943bee4d693ff724e4b9712357df9dd30a12c075
PNPtv Support <support@pnptv.app>
2025-08-29 00:54:32 +0000

    is this ready for deployment?

--- next.config.ts
+++ next.config.ts
@@ -12,14 +12,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
-      }
+      },
     ],
   },
 };
